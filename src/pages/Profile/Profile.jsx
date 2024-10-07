8d4aeba7e473ec0ad27929fcc60a6d355f71a7c7
beerser <bearjaiton@gmail.com>
2024-10-07 17:39:54 +0700

    v2-5

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import './Profile.css'
-import Navbar from '../../components/Navbar/Navbar'
 
 function Profile() {
   return (
-  )
-}
-
+    <div className="profile-container">
+      <div className="profile-picture">
+        <img src="path_to_profile_picture.jpg" alt="Profile" />
+        <button className="upload-btn">Upload</button>
+      </div>
+      <div className="profile-details">
+        <h1 className="username">Krijtat Sinrungtham</h1>
+        <p className="user-info">krijtat@example.com | Bangkok, Thailand</p>
+        <div className="action-buttons">
+          <button className="edit-btn">Edit Profile</button>
+          <button className="view-btn">View Full Profile</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default Profile
 
